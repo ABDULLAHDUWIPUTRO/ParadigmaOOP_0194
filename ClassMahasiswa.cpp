d14730d3a5f97821ea5a9625b5e4b1aa4de122e2
ABDULLAHDUWIPUTRO <abdullahduwiputro69@gmail.com>
2026-04-28 09:43:33 +0700

    Method untuk menampilkan data

--- ClassMahasiswa.cpp
+++ ClassMahasiswa.cpp
@@ -4,3 +4,14 @@ using namespace std;
 class Mahasiswa
 {
     public: //akses modifier
+        int NIM;
+        string nama;
+        float nilai;
+
+    void printData()
+    {
+        cout << "NIM: " << NIM << endl;
+        cout << "NAMA: " << nama << endl;
+        cout << "NILAI: " << nilai << endl; 
+    }
+};
