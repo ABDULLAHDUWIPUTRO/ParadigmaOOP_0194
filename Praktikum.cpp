2f7978eff52a4a88b653c8f57f37d60c3be8d8df
ABDULLAHDUWIPUTRO <abdullahduwiputro69@gmail.com>
2026-04-28 09:56:08 +0700

    Implementasi method untuk menampilkan data

--- Praktikum.cpp
+++ Praktikum.cpp
@@ -10,6 +10,13 @@ public:
     string kategori;
     string tanggalProduksi;
 
-
+    // Commit 5: Implementasi method untuk menampilkan data
     void tampilkanSpesifikasi() {
-   
+        cout << "=== Spesifikasi Barang ===" << endl;
+        cout << "Nama Barang      : " << nama << endl;
+        cout << "Jumlah           : " << jumlah << endl;
+        cout << "Kategori         : " << kategori << endl;
+        cout << "Tanggal Produksi : " << tanggalProduksi << endl;
+        cout << "--------------------------" << endl;
+    }
+};
